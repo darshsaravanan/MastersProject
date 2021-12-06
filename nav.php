72acf41bd141ea34d00d290afc13c8c047b4d7c1
darshsaravanan <77697540+darshsaravanan@users.noreply.github.com>
2021-12-06 18:16:33 -0500

    Update nav.php

--- nav.php
+++ nav.php
@@ -1,5 +1,6 @@
-<link rel="stylesheet" href="static/css/style.css">
+<!--<link rel="stylesheet" href="static/css/style.css">
 <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+-->
 <?php
 require_once(__DIR__ . "/../lib/helpers.php");
 ?>
